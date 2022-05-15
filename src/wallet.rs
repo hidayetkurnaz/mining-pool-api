f3bdc39822233ea58c7e69387915b6e1f64779bb
hidayetkurnaz <hidayetkurnaz@gmail.com>
2022-05-15 17:04:45 +0300

    wallet created

---
+++ src/wallet.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+// ------------------- JSON Payload (REST)
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Wallet {
+    pub address: String,
+    pub club_name: String,
+    pub total_hash_rate: i32, //MH/s
+    pub total_shares_mined: i32,
+    pub total_workers_online: i32,
+    pub workers_online: Vec<Miner>,
+}
+
+// ------------------- POST Request Body for new Miner
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewWalletRequest {
+    club_name: String,
+}
+
+
+// ------------------- DAO(database access object) Object (DB Table Records)
+
+pub struct Wallet DAO {
+    pub address: String,
+    pub club_name: String,
+}
